066be6c0a29894ceac107ef42a379f7970e58133
James Robertson <james@r0bertson.co.uk>
2017-09-17 15:36:29 +0100

    bug fix: Added a File.join routine which had previously been deleted in error.

--- lib/testdata_text.rb
+++ lib/testdata_text.rb
@@ -5,20 +5,24 @@
 require 'rowx'
 require 'nokogiri'
 
-    
+class String
+  def /(str_to_join)
+    File.join(self, str_to_join)
+  end
+end
+
 class TestdataText
 
   def self.parse(raw_txt)
 
     txt = raw_txt.split(/(?=\n=begin)/m)\
-                                .map {|x| x.sub(/\n=begin.*=end/m,'')}.join
-
+                                .map {|x| x.sub(/\n=begin.*=end/m,'')}.join    
     rowx = RowX.new(txt, level: 1, ignore_blank_lines: true, abort_1_row: true)
     xml = rowx.to_xml
 
     lib = File.dirname(__FILE__)
     xsl = File.open(lib/'testdata.xsl','r').read
-    
+
     doc = Nokogiri::XML(xml)
     xslt  = Nokogiri::XSLT(xsl)
     xslt.transform(doc).to_s
